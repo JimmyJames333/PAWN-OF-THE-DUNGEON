e2643bb5ed782a2d0df78055cb6d8b463e502efb
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-13 23:51:48 +0930

    Update GameScene.js

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -1,29 +1,13 @@
 class GameScene extends Phaser.Scene {
-    constructor() {
-        super("GameScene");
-    }
+  constructor() {
+    super('GameScene');
+  }
 
-    create() {
-        this.mapGen = new MapGenerator(this);
-        this.mapGen.generateDungeon();
-
-        this.corruption = new CorruptionSystem(this);
-        this.synergy = new SynergySystem(this);
-
-        this.player = new Player(this, 640, 360);
-        this.enemy = new Enemy(this, 900, 360);
-
-        this.physics.add.overlap(
-            this.player.sprite,
-            this.enemy.sprite,
-            () => this.enemy.takeDamage(1)
-        );
-
-        this.scene.launch("UIScene");
-    }
-
-    update() {
-        this.player.update();
-        this.enemy.update();
-    }
+  create() {
+    const { width, height } = this.scale;
+    this.add.text(width / 2, height / 2, 'Game Started!', {
+      fontSize: '32px',
+      color: '#00ff00'
+    }).setOrigin(0.5);
+  }
 }
