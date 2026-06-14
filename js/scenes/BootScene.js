1574a05f10330dd7e9017358107494660028d8b2
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-14 18:55:59 +0930

    Update BootScene.js

--- js/scenes/BootScene.js
+++ js/scenes/BootScene.js
@@ -1,24 +1,14 @@
 class BootScene extends Phaser.Scene {
-  constructor() {
-    super('BootScene');
-  }
+    constructor() {
+        super("BootScene");
+    }
 
-  preload() {
-    // Optional logo, comment out if you don't have it
-    // this.load.image('logo', 'assets/sprites/logo.png');
-  }
+    preload() {
+        // Load normal chess pawn from assets folder
+        this.load.image("pawn", "assets/pawn.png");
+    }
 
-  create() {
-    const { width, height } = this.scale;
-
-    this.add.text(width / 2, height / 2, 'Loading...', {
-      fontSize: '32px',
-      color: '#ffffff'
-    }).setOrigin(0.5);
-
-    this.time.delayedCall(1000, () => {
-      console.log('BootScene finished loading, switching to CharacterSelectScene...');
-      this.scene.start('CharacterSelectScene');
-    });
-  }
+    create() {
+        this.scene.start("CharacterSelectScene");
+    }
 }
